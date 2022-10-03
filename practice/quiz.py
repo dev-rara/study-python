74056428536146bfaf336eaebc02c9d609d848bb
rara-dev <kksr1219@gmail.com>
2022-10-03 23:55:58 +0900

    python quiz

--- practice/quiz.py
+++ practice/quiz.py
@@ -1,13 +1,11 @@
 num_list = [1, 2, 3, 6, 3, 2, 4, 5, 6, 2, 4]
 
-# Q1
+# Q1. 짝수 출력하기
 for num in num_list:
     if num % 2 == 0:
         print(num)
-        
-print('===== 구분선 =====')
 
-# Q2
+# Q2. 짝수의 개수 출력하기
 count = 0
 for num in num_list:
     if num % 2 == 0:
@@ -15,19 +13,20 @@ for num in num_list:
 
 print(count)
 
-print('===== 구분선 =====')
 
-# Q3
-total = 0
+# Q3. 모든 숫자 더하기
+sum = 0
 for num in num_list:
-    total += num
-print(total)
+    sum += num
+    
+print(sum)
 
-print('===== 구분선 =====')
 
-# Q4
+# Q4. 가장 큰 숫자 구하기
 max_num = 0
 for num in num_list:
     if num >= max_num:
         max_num = num
+        
 print(max_num)
+print(max(num_list))   # max()를 사용하면 간단하다
